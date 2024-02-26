752f28fc949a967d5382df0057eed1538dabb0e0
Shohag Rana <94190576+shohagranasuvo@users.noreply.github.com>
2024-02-26 16:12:47 +0600

    Create Area.cpp
    area trapezio,triangle,circle,

---
+++ Area.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+ using namespace std;
+int main() {
+ double a,b,c;
+
+ cin>>a>>b>>c;
+
+cout <<fixed<<setprecision(3)<<"TRIANGULO: "<<0.5*a*c<<endl;
+
+ cout <<fixed<<setprecision(3)<<"CIRCULO: "<<3.14159*c<<endl;
+
+ cout <<fixed<<setprecision(3)<<"TRAPEZIO: "<<0.5*(a+b)*c<<endl;
+
+ cout <<fixed<<setprecision(3)<<"QUADRADO: "<<b*b<<endl;
+
+ cout <<fixed<<setprecision(3)<<"RETANGULO: "<<a*b<<endl;
+
+
+
+
+
+    return 0;
+}
