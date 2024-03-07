424aeb030943da2461f0494bc1d5a61b2977a174
Shohag Rana <94190576+shohagranasuvo@users.noreply.github.com>
2024-03-08 01:31:41 +0600

    Create Namesquare.cpp

---
+++ Namesquare.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+    char name[20];
+
+    cout << "Enter your name: ";
+    cin.getline(name, 20);
+
+    int length = strlen(name);
+
+
+      for (int row = 1; row <= length; row++)
+        {
+
+        for (int space = length; space > row; space--) {
+            cout << " ";
+        }
+
+        for (int i = 0; i < row; i++) {
+            cout << name[i];
+
+        }
+
+        for (int i = 0; i < row; i++) {
+            cout << name[i];}
+
+
+
+
+        cout << endl;
+    }
+
+
+
+
+
+
+
+    for (int row = length - 1; row >= 1; row--) {
+
+        for (int space = length; space > row; space--) {
+            cout << " ";
+        }
+
+        for (int i = 0; i < row; i++) {
+            cout << name[i];
+        }
+
+         for (int i = 0; i < row; i++) {
+            cout << name[i];
+        }
+        cout << endl;
+    }
+
+    return 0;
+}
